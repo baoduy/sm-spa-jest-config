5195b1c397fbac880b5fe235816b699f743dd8a6
Steven Hoang <drunkcoding@outlook.com>
2018-08-23 22:52:12 +0800

    improvement

--- testSetup.js
+++ testSetup.js
@@ -7,3 +7,32 @@ enzyme.configure({ adapter: new Adapter() });
 //Setup Jest
 import { createSerializer } from 'enzyme-to-json';
 expect.addSnapshotSerializer(createSerializer({ mode: 'deep' })); //deep,shallow
+
+//mock storage
+//browserMocks.js
+const localStorageMock = (function() {
+  var store = {};
+
+  return {
+    getItem: function(key) {
+      return store[key] || null;
+    },
+    setItem: function(key, value) {
+      store[key] = value;
+    },
+    removeItem: function(key) {
+      store[key] = undefined;
+    },
+    clear: function() {
+      store = {};
+    }
+  };
+})();
+
+Object.defineProperty(window, 'localStorage', {
+  value: localStorageMock
+});
+
+Object.defineProperty(window, 'sessionStorage', {
+  value: localStorageMock
+});
